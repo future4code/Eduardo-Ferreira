b9c4a3ad9fb74814f5cb53eeb8739a2fd1ba6191
Eduardo Silva <eduardo.sf.1992@gmail.com>
2021-02-26 17:11:29 -0300

    Semana: blackjack

---
+++ semana3/blackjack/desafio.js
@@ -0,0 +1,71 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+/* let start = confirm("Deseja iniciar o jogo?");
+
+class Player {
+   constructor(name)
+   {
+      this.name = name;
+      this.cards = [comprarCarta(), comprarCarta()];
+   }
+
+   newCard(){
+      this.cards.push(comprarCarta());
+      return true;
+
+   }
+
+   score(){
+      let sum = 0;
+      let i = 0;
+      while(i < this.cards.length){
+         sum += this.cards[i].valor;
+         i++;
+      }
+      return sum;
+   }
+}
+
+function setupCard(id, card){
+   for (var i = 1; i <= 5; i++) {
+      dealerCards[i] = new DisplayedCard("dealer" + i);
+      playerCards[i] = new DisplayedCard("player" + i);
+   }
+}
+
+function mapCard(){
+   this.down = true;
+   this.div = document.getElementById(id);
+}
+
+const user = new Player("Eduardo");
+const computer = new Player("Computador");
+//Dar Cartas
+
+const newGame = document.getElementById("newgame");
+const user = new Player("Eduardo");
+const computer = new Player("Computador");
+
+newGame.addEventListener('click', (event) => {
+   
+   console.log("Eduardo");
+});
+
+if(start) {
+   
+} else {
+	console.log("O jogo acabou!");
+} */
+
+//
