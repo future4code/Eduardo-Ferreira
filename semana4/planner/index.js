190e7ed0b74af7fff359f0c9bafc7e80f2b77b5b
Eduardo Silva <eduardo.sf.1992@gmail.com>
2021-03-02 17:43:12 -0300

    Semana: trabalhando com o DOM -> atualizado

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -4,20 +4,38 @@ function createTask() {
     const task = form["task"].value;
     const selectWeek = form["dias-semana"].value;
     const divWeek = document.getElementById(`${selectWeek}`);
-    divWeek.innerHTML += "<p class=" + "p" + ">" + task + "</p>";
-
     const p = document.getElementsByClassName('p');
 
-    for (let element of p) {
-        element.addEventListener('click', (event) => {
-            element.style.textDecoration = "line-through";
-        });
+    if (task == "") {
+        alert("Não é possível cadastrar uma tarefa em branco!");
+        return false;
+    } else {
+        divWeek.innerHTML += "<p class=" + "p" + ">" + task + "</p>";
+        for (let element of p) {
+            element.addEventListener('click', (event) => {
+                element.style.textDecoration = "line-through";
+            });
+        }
     }
 }
 
-button.addEventListener('click', (event) => {
-    event.preventDefault();
+const create = document.getElementById('create');
+const clear = document.getElementById('clear');
 
+create.addEventListener('click', (event) => {
+    event.preventDefault();
+    
     createTask();
 
+});
+
+clear.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const listTasks = document.querySelectorAll('div.list-task');
+    let i = 0;
+    while(i < listTasks.length){
+        listTasks[i].innerHTML = "";
+        i++;
+    }
 });
