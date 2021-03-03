d75538446706eb316a7db6d47bbc25aa403f2e26
Eduardo Silva <eduardo.sf.1992@gmail.com>
2021-03-03 18:41:06 -0300

    semana4: index.js atualizado

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,40 +1,3 @@
-/**  */
-
-
-const arrPosts = [];
-
-function pegarDados() {
-    const form = document.forms[0];
-    let titulo = form.titulo.value;
-    let img = form.imagem.value;
-    let autor = form.autor.value;
-    let conteudo = form.conteudo.value;
-
-    if((titulo == "") || (autor == "") || (conteudo == "") || (img == "")){
-        alert("Preencha todos os campos!");
-        return false;
-    }
-
-    arrPosts.push({
-        id: arrPosts.length + 1,
-        titulo: titulo.value,
-        autor: autor.value,
-        img: img.value,
-        conteudo: conteudo.value
-    });
-
-    const post = document.createElement("div");
-    const posts = document.querySelector(".posts");
-
-    post.innerHTML = "<h1>" + titulo + "</h1><span>"+"<img src="+ img +">" + "<i>" + autor + "</i>" + "</span><p>" + conteudo + "</p>";
-    posts.appendChild(post);
-
-    titulo.value = "";
-    autor.value = "";
-    conteudo.value = "";
-    //passaValor(titulo, img, autor, conteudo);
-}
-
 /* let passaValor = function(titulo, img, autor, conteudo)
 {
     window.location = "posts.html?titulo="+titulo+"?img="+img+"?autor="+autor+"?conteudo="+conteudo;
@@ -51,3 +14,49 @@ function pegarDados() {
 }
 
 variaveisUrl(); */
+
+const arrPosts = [];
+//criar objeto
+//imprimir objeto
+const form = document.forms[0];
+let titulo = form.titulo;
+let img = form.imagem;
+let autor = form.autor;
+let conteudo = form.conteudo;
+
+function criarPost() {
+
+    if ((titulo.value == "") || (autor.value == "") || (conteudo.value == "") || (img.value == "")) {
+        alert("Preencha todos os campos!");
+        return false;
+    }
+
+    const obj = {
+        id: arrPosts.length + 1,
+        titulo: titulo.value,
+        autor: autor.value,
+        img: img.value,
+        conteudo: conteudo.value
+    }
+    
+    arrPosts.push({obj});
+
+    imprimirPost(obj.titulo, obj.autor, obj.img, obj.conteudo);
+}
+
+function imprimirPost(titulo, autor, img, conteudo) {
+
+    const post = document.createElement("div");
+    const posts = document.querySelector(".posts");
+
+    post.innerHTML = "<h1>" + titulo + "</h1><span>" + "<img src=" + autor + ">" + "<i>" + img + "</i>" + "</span><p>" + conteudo + "</p>";
+    posts.appendChild(post);
+    console.log("Eduardo");
+
+    console.log(arrPosts);
+}
+
+//adicionar ID ao HTML
+
+//quando clicar no link executar uma função que vai pegar 
+
