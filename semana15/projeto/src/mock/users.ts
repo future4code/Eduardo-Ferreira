7558df0d5ca8cdefeab94d724bcd55c5d8443624
EDUDSF <eduardo.sf.1992@gmail.com>
2021-05-21 18:18:35 -0300

    index criado

---
+++ semana15/projeto/src/mock/users.ts
@@ -0,0 +1,42 @@
+type User = {
+  name: string
+  doc: string
+  birthday: string
+  balance: number
+}
+
+// Interage com pagar conta, adicionar saldo e transferência interna
+type Extract = {
+  value: number
+  date: object
+  description: string
+}
+
+export const users: User[] = [
+  {
+    "name": "teste",
+    "doc": "016.640.800-07",
+    "birthday": "01/12/1958",
+    "balance": 0
+  },
+  {
+    "name": "teste2",
+    "doc": "016.640.800-07",
+    "birthday": "01/12/1958",
+    "balance": 0
+  },
+  {
+    "name": "teste3",
+    "doc": "016.640.800-07",
+    "birthday": "01/12/1958",
+    "balance": 0
+  },
+  {
+    "name": "teste4",
+    "doc": "016.640.800-07",
+    "birthday": "01/12/1958",
+    "balance": 0
+  }
+]
+
+//
